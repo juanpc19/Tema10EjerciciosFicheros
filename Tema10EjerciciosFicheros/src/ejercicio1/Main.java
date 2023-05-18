de4f6703f376d9988d7a522df639166ac64fdc71
juanpc19 <juan.perez@iesnervion.es>
2023-05-18 13:47:59 +0200

    a

--- Tema10EjerciciosFicheros/src/ejercicio1/Main.java
+++ Tema10EjerciciosFicheros/src/ejercicio1/Main.java
@@ -1,5 +1,6 @@
 package ejercicio1;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -9,23 +10,40 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		FileWriter escribir = null;
+		BufferedReader fichero = null;
+
+		double suma;
+
+		int contador;
+
+		double mediaAritmetica;
+
+		String linea = "";
+
+		String numerosLinea[];
 
 		try {
-			escribir = new FileWriter("C:\\Escritorio\\NumerosReales.txt");
-			for (int i =0; i<6;i++) {
-				escribir.write(1);
+			fichero = new BufferedReader(new FileReader("src//ejercicio1"));
+			linea = fichero.readLine();
+
+			while (linea != null) {
+				linea = fichero.readLine();
+				numerosLinea = linea.split(" ");
+				//for each
 			}
-			
-		} catch (IOException e) {
-			System.err.println(e.getMessage());
+
 		} finally {
-			escribir.flush();
-			escribir.close();
+			fichero.close();
 		}
 
-		
-		
+		/*
+		 * FileWriter escribir = null; try { escribir = new
+		 * FileWriter("src//ejercicio1"); for (int i =0; i<6;i++) { escribir.write(1); }
+		 * 
+		 * } catch (IOException e) { System.err.println(e.getMessage()); } finally {
+		 * escribir.flush(); escribir.close(); }
+		 */
+
 	}
 
 }
